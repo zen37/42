1fab5638b5b350f2afeeffc4ad0659d669c3cf3f
zen <mobi2009ro@gmail.com>
2023-12-13 13:28:54 -0800

    refactor audio

--- 1-hello_world/audio.py
+++ 1-hello_world/audio.py
@@ -3,7 +3,6 @@
 """
 
 import os
-import json
 import random
 import azure.cognitiveservices.speech as speechsdk
 
@@ -11,25 +10,13 @@ import azure.cognitiveservices.speech as speechsdk
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 
-from helpers import get_config_service, get_key_speech
+from helpers import get_config_service, get_key_speech, log_function_call
 from constants import DEFAULT_VOICE, DIRECTORY_AUDIO, ENCODING
 
 
 def get_available_voices():
-    """gets the available voices list"""
-
-    config = get_config_service("azure")
-
-    REGION = config["region"]
-    KEY = get_key_speech()
-
-    print("KEY", KEY)
-
-#speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
-    speech_config = speechsdk.SpeechConfig(subscription=KEY, region=REGION)
-
-    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config , audio_config=None)
-
+    """gets the available voices list as provided by transkaotor service"""
+    speech_synthesizer = get_speech_synthesizer()
     # request the list of available voices
     result = speech_synthesizer.get_voices_async().get()
     if result.reason == speechsdk.ResultReason.VoicesListRetrieved:
@@ -47,7 +34,6 @@ def get_voice(language_code):
     for voice in voices:
         #print(voice.__dict__)  #inspect attributes of voice object
         if (voice.locale[:2] == language_code):
-            #print(voice.short_name)
             voice_names.append(voice.short_name)
 
     if voice_names:
@@ -59,7 +45,6 @@ def get_voice(language_code):
 def get_filepath(language_code, filename):
     """gets path to audio file"""
     language_folder = os.path.join(DIRECTORY_AUDIO, language_code)
-    print("language_folder", language_folder)
     os.makedirs(language_folder, exist_ok=True)
     filepath = os.path.join(language_folder, filename)
 
@@ -90,6 +75,10 @@ def get_audio_file(language_code):
     else:
         # Get all files that start with language_code
         path = os.path.join(DIRECTORY_AUDIO, language_code)
+        if not os.path.exists(path):
+            os.makedirs(path)
+            return None  # just created the directory, obviously there is no file
+
         files = [f for f in os.listdir(path) if f.startswith(language_code)]
 
         print(f"audio files found locally for '{language_code}': {len(files)}")
@@ -106,11 +95,35 @@ def get_audio_file(language_code):
             return None
 
 
+def get_speech_config(voice=None):
+
+    config = get_config_service("azure")
+    REGION = config["region"]
+    KEY = get_key_speech()
+    print("key:", KEY)
+
+    #speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+    speech_config = speechsdk.SpeechConfig(subscription=KEY, region=REGION)
+    if voice:
+        speech_config.speech_synthesis_voice_name = voice
+
+    return speech_config
+
+
+def get_speech_synthesizer(voice=None, audio_config=None):
+    speech_config       = get_speech_config(voice)
+    if audio_config:
+        speech_synthesizer  = speechsdk.SpeechSynthesizer(speech_config=speech_config , audio_config=audio_config)
+    else:
+        speech_synthesizer  = speechsdk.SpeechSynthesizer(speech_config=speech_config , audio_config=None)
+
+    return speech_synthesizer
+
+
 def talk(language_locale, text):
     """speaks the text in the provided language"""
 
     language_code = language_locale[:2]
-
     voice = get_audio_file(language_code)
 
     if voice:
@@ -118,15 +131,11 @@ def talk(language_locale, text):
         play(filepath)
     else:
         voice = get_voice(language_code)
-
+        print("voice: ", voice)
         filepath = get_filepath(language_code, voice)
-
+        print("filepath: ", filepath)
         audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True, filename = filepath)
-
-        speech_config.speech_synthesis_voice_name=voice
-
-        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-
+        speech_synthesizer = get_speech_synthesizer(voice, audio_config)
         speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
 
         if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
@@ -134,7 +143,7 @@ def talk(language_locale, text):
             #the code is from Microsoft Learn, not an excuse as for sure I would have not known to do it by myself anyway
             #print("Speech synthesized for text [{}]".format(text))
             print(f"speech synthesized for voice [{voice}] and text [{text}]")
-            filepath = get_filepath(voice)
+            filepath = get_filepath(language_code, voice)
             if os.path.isfile(filepath):
                 play(filepath)
         elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
--- 1-hello_world/helpers.py
+++ 1-hello_world/helpers.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 import json
 
+import inspect
+
 from constants import ENCODING, DIR_CONFIG, FILE_COMMON_CONFIG, FILE_AZURE_CONFIG
 
 def set_locale(language, encoding):
@@ -14,14 +16,20 @@ def set_locale(language, encoding):
         return None
 
 
+def log_function_call(func):
+    def wrapper(*args, **kwargs):
+        caller = inspect.currentframe().f_back.f_code.co_name
+        print(f"Function {func.__name__} called by {caller} with args: {args}, kwargs: {kwargs}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} returned: {result}")
+        return result
+    return wrapper
+
 
 def get_config():
-
     config_path = os.path.join(DIR_CONFIG, FILE_COMMON_CONFIG)
-
     with open(config_path, "r", encoding = ENCODING) as file:
         config = json.load(file)
-
     return config
 
 
@@ -40,14 +48,15 @@ def get_config_service(provider):
 def load_environment_variables():
     config = get_config()
     service = config.get("translation_service", "").lower()
+    secrets_path = os.path.join("env", f"{service}.env")
 
-    secrets_path = os.path.join("env/", service + ".env")
-    load_dotenv(dotenv_path=secrets_path )
+    load_dotenv(dotenv_path=secrets_path)
 
 
 def get_key_translation():
-    key = os.getenv("KEY_TRANSLATION")
+    key = os.getenv("KEY_TRANSLATOR")
     return key
+
 
 def get_key_speech():
     key = os.getenv("KEY_SPEECH")

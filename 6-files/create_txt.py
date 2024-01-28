054f526a3fc9d47c8886d71216aad1be53f2a300
zen <zen@zen>
2024-01-28 05:02:47 -0800

    countries

--- 6-files/create_txt.py
+++ 6-files/create_txt.py
@@ -8,13 +8,14 @@ def create(id, order):
         for record in records:
             # Remove newline character from the end of the record
             record = record.strip()
-            line = f"((byte){id}, \"{record}\", (byte){order}),"
+            #line = f"((byte){id}, \"{record}\", (byte){order}),"
+            line = f"({id}, \"{record}\", {order}),"
             # Write the processed record to output.txt
             output_file.write(line + "\n")
             print(line)
             # Increment id and order for the next record
-            id += 1
-            order += 1
+            id      += 1
+            order   += 1
 
 if __name__ == "__main__":
     create(1, 41)

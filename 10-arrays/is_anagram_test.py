cb849d5c3185af5a717d95b5cd7d0aedebfe7c3d
zen <mobi2009ro@gmail.com>
2024-05-29 19:50:46 -0700

    solution

--- 10-arrays/is_anagram_test.py
+++ 10-arrays/is_anagram_test.py
@@ -1,11 +1,20 @@
-from is_anagram import *
+from is_anagram import Solution
 
-def test():
+def test_is_anagram():
     test_cases = [
         (1, 'anagram', 'nagaram', True),
-        (2, 'rat', 'car', False)
+        (2, 'rat', 'car', False),
+        (3, 'a', 'ab', False),
+        (4, 'a', 'aa', False),
+        (5, 'aacc', 'ccac', False),
+        (6, 'aa cc d', 'dc ca a', True),
+        (7, 'aa ccd', 'dc ca  a', False)
     ]
 
     for num, str1, str2, expected in test_cases:
-        solution = Solution()
-        assert solution.is_anagram(str1, str2) == expected, f"failed test #{num}: {str1} vs. {str2}"
+        s = Solution()
+        assert s.is_anagram(str1, str2) == expected, f"failed test #{num}: {str1} vs. {str2}"
+
+
+if __name__ == "__main__":
+    test_is_anagram()

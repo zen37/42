4b3aabc9f4caecfb4e8549027eeff9d221b4e400
zen <zen@talk-talk.net>
2024-07-01 08:55:57 -0700

    find missing number

---
+++ 10-arrays/find_missing_number_test.py
@@ -0,0 +1,23 @@
+from find_missing_number import Solution
+
+test_cases = [
+    ([3, 2, 1, 5], 4),
+    ([3, 7, 1, 2, 8, 4, 6], 5),
+    ([3, 1], 2),
+    ([2, 1], None),
+    ([1], None)
+]
+
+def test_find_missing_number_1():
+    s = Solution()
+    for test in  test_cases:
+        expected = test[1]
+        result = s.find_using_sum(test[0])
+        assert result == expected, f"FAIL {result} vs. {expected}"
+
+def test_find_missing_number_2():
+    s = Solution()
+    for test in  test_cases:
+        expected = test[1]
+        result = s.find_using_set(test[0])
+        assert result == expected, f"FAIL {result} vs. {expected}"

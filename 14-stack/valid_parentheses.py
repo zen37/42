ebb1211d3d61388120b1c54707621bb05fb1fabd
zen <zen@talk-talk.net>
2024-07-02 20:07:02 -0700

    valid parentheses

---
+++ 14-stack/valid_parentheses.py
@@ -0,0 +1,20 @@
+class Solution():
+    def isValid(self, s: str) -> bool:
+        map = {")": "(", "]": "[", "}": "{"}
+        list = []
+
+        for c in s:
+            if c not in map:
+                list.append(c)
+                continue
+            if not list or list[-1] != map[c]:
+                return False
+            list.pop()
+
+        return not list
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isValid("{}"))
+    print(s.isValid("(]"))
+    print(s.isValid("()[]{}"))
